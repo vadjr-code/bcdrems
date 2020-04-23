c12967492a73448600dcb4035f1aa41e2edaa26d
vadjr-code <github.boyd@gmail.com>
2020-04-23 05:59:21 -0600

    Updated some files

--- web_config.php
+++ web_config.php
@@ -4,13 +4,13 @@ INFO: This is where you setup the your database credentials */
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'rems');
+define('DB_NAME', 'bcdrems');
 
 /* SITE SETTINGS
 INFO: You can change this depending on your needs but stay
 this as default if you don't know what you are doing */
-define('SITE_TITLE', 'REMS');
-define('SITE_DOMAIN', 'http://rems.dcom/rems');
+define('SITE_TITLE', 'BCDREMS');
+define('SITE_DOMAIN', 'http://localhost/bcdrems');
 define('SITE_ADMIN_DIR', SITE_DOMAIN . '/admin');
 define('SITE_CORE_DIR', SITE_DOMAIN . '/core');
 define('SITE_FRONT_END_DIR', SITE_DOMAIN . '/front-end');
