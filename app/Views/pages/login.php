28b50db9c71f1169c4ca5f7ace2af1ea52fdc1a5
vadjr-code <github.boyd@gmail.com>
2020-04-26 08:02:49 -0600

    Modify Form Methods

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -16,22 +16,16 @@
 									<div class="text-center">
 										<h1 class="h4 text-gray-900 mb-4">Real Estate Management</h1>
 									</div>
-									<form class="user">
+									<form method="POST" action="/login" class="user">
 										<div class="form-group">
 											<input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." autocomplete="off">
 										</div>
 										<div class="form-group">
 											<input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" autocomplete="off">
 										</div>
-										<div class="form-group">
-											<div class="custom-control custom-checkbox small">
-												<input type="checkbox" class="custom-control-input" id="customCheck">
-												<label class="custom-control-label" for="customCheck">Remember Me</label>
-											</div>
-										</div>
-										<a href="index.html" class="btn btn-primary btn-user btn-block">
+										<button class="btn btn-primary btn-user btn-block">
 											Login
-										</a>
+										</button>
 									</form>
 									<hr>
 									<div class="text-center">
