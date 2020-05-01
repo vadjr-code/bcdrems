035e9115e51341c5e3aaf665f1973244a494ba09
vadjr-code <github.boyd@gmail.com>
2020-05-01 19:24:59 +0800

    Add Quick link feature to tinymce plugin

--- inc/assets/mgmt/js/scripts.js
+++ inc/assets/mgmt/js/scripts.js
@@ -13,6 +13,7 @@
 		height: 350,
 		plugins: "link",
 		default_link_target: "_blank",
-		toolbar: 'undo redo bold italic link'
+		toolbar: 'undo redo bold italic link',
+		link_quicklink: true
 	});
 })(jQuery);
